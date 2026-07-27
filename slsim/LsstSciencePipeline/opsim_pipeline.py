fba27e307b12028c300b4359c142610babe63242
Simon Birrer <sibirrer@users.noreply.github.com>
2026-07-27 14:33:16 -0400

    attempt to fix precommit

--- slsim/LsstSciencePipeline/opsim_pipeline.py
+++ slsim/LsstSciencePipeline/opsim_pipeline.py
@@ -17,23 +17,17 @@ def opsim_time_series_images_data(
     print_warning=True,
 ):
     """Creates time series data from opsim database.
+    The survey cadence files for Opsim can be found at
+    https://s3df.slac.stanford.edu/data/rubin/sim-
+        data/sims_featureScheduler_runsX.X/baseline/
+    folder. (replace X.X with the version of the opsim database you want to
+        use).
 
-    :param ra_list: a list of ra points (in degrees) from objects we
-    want to collect observations for :param dec_list: a list of dec
-    points (in degrees) from objects we     want to collect observations
-    for :param opsim_path: string, path to the opsim database. The opsim
-    database can be downloaded from
-    https://s3df.slac.stanford.edu/data/rubin/sim-data/sims_featureScheduler_runsX.X/baseline/
     :param ra_list: a list of ra points (in degrees) from objects we
         want to collect observations for
     :param dec_list: a list of dec points (in degrees) from objects we
         want to collect observations for
-    :param opsim_path: string, path to the opsim database. The opsim
-        database can be downloaded from
-        https://s3df.slac.stanford.edu/data/rubin/sim-
-        data/sims_featureScheduler_runsX.X/baseline/ # noqa folder
-        (replace X.X with the version of the opsim database you want to
-        use).
+    :param opsim_path: string, path to the opsim database.
     :param MJD_min: minimum MJD for the observations
     :param MJD_max: maximum MJD for the observations
     :param num_pix: cutout size of images (in pixels)
