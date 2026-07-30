e988a7528654433c33eaab3af7ffe7f95b006d9b
Simon Birrer <sibirrer@users.noreply.github.com>
2026-07-30 09:33:22 -0400

    minor bug fix in shifting observation times to first arriving image

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
-#!/usr/bin/env python
-
-"""The setup script."""
-
 from setuptools import setup, find_packages
+"""The setup script."""
 
 with open("README.rst") as readme_file:
     readme = readme_file.read()
